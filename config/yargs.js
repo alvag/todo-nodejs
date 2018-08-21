2298f95d87ca95a9a9b5af766323673c4a6b7a89
Max Alva <alva85@gmail.com>
2018-08-21 12:55:29 -0500

    Optimizacion

--- config/yargs.js
+++ config/yargs.js
@@ -1,30 +1,19 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion del ToDo'
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado el ToDo'
+};
+
 const argv = require( 'yargs' )
-                    .command('crear', 'Crear ToDo', {
-                        descripcion: {
-                            demand: true,
-                            alias: 'd',
-                            desc: 'Descripcion del ToDo'
-                        }
-                    })
-                    .command('actualizar', 'Actualiza el estado del ToDo', {
-                        descripcion: {
-                            demand: true,
-                            alias: 'd',
-                            desc: 'Descripcion del ToDo'
-                        },
-                        completado: {
-                            default: true,
-                            alias: 'c',
-                            desc: 'Marca como completado el ToDo'
-                        }
-                    })
-                    .command('eliminar', 'Elimina un ToDo', {
-                        descripcion: {
-                            demand: true,
-                            alias: 'd',
-                            desc: 'Descripcion del ToDo'
-                        }
-                    })
+                    .command('crear', 'Crear ToDo', { descripcion })
+                    .command('actualizar', 'Actualiza el estado del ToDo', { descripcion, completado})
+                    .command('eliminar', 'Elimina un ToDo', { descripcion })
                     .help()
                     .argv;
 
